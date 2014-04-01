1a1451818dfd372a61a71ab31ff23ce5c79058af
Leigh Dodds <leigh@ldodds.com>
2014-04-01 11:29:58 +0100

    Implement remaining key features on observation page

--- features/observation_page.feature
+++ features/observation_page.feature
@@ -5,10 +5,18 @@ Feature: Observation page
 
   Scenario: get observation page
     Given the fixture "mca5-2013may.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds/observations/mca5-2013may.json"
+    And the fixture "ppi-csdb-ds.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds.json"
+    And the fixture "2014-02-18.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18.json"
+    And the fixture "producer-price-index.json" is available at the URL "https://data-api.com/series/producer-price-index.json"
+    And the fixture "empty.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds/observations.json?date=2013APR&product=MCA5"
+    And the fixture "empty.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds/observations.json?date=2013JUN&product=MCA5"
+    And the fixture "empty.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds/observations.json?date=2012MAY&product=MCA5"
+    And the fixture "empty.json" is available at the URL "https://data-api.com/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds/observations.json?date=2014MAY&product=MCA5"
+
     When I go to the path "/series/producer-price-index/releases/2014-02-18/datasets/ppi-csdb-ds/observations/mca5-2013may"
     Then I should see "106.2" within "div.observation p.main"
     And I should see "date" within "table"
     And I should see "2013 MAY" within "table"
     And I should see "product" within "table"
     And I should see "Input component - Imported products used in the Manufacture of" within "table"
-    And I should see "In 2013 MAY, the price index for Input component - Imported products used in the Manufacture of was..." within "p.lead"
+    And I should see "In 2013 MAY, the price_index for Input component - Imported products used in the Manufacture of was..." within "p.lead"
--- ons_frontend.rb
+++ ons_frontend.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/content_for'
 require 'sinatra/partial'
+require 'bigdecimal'
 require_relative 'models/all'
 
 class OnsFrontend < Sinatra::Base
@@ -11,7 +12,7 @@ class OnsFrontend < Sinatra::Base
   enable :partial_underscores
   
   get '/' do
-    'Hello OnsFrontend!'
+    erb :index
   end
 
   get '/series/:series/releases/:release/datasets/:dataset/observations/:observation' do
@@ -20,10 +21,44 @@ class OnsFrontend < Sinatra::Base
                                               release_id: params[:release],
                                               dataset_id: params[:dataset]}
                                    )
-    @dataset = @observation.dataset
-    @release = @observation.dataset.release
-    @series = @observation.dataset.release.series
-    erb :observation
+
+
+                                   
+    @related = find_related_observations( @observation )         
+            
+    @dataset = Dataset.find(params[:dataset], 
+                params: { release_id: params[:release], 
+                          series_id: params[:series] 
+                })
+                
+    @release = Release.find( params[:release], params: { series_id: params[:series] })            
+    @series = Series.find( params[:series])
+    @measure = @observation.measures.first.slug
+    erb :"observation/index"
+  end
+  
+  def find_related_observations( observation )
+    related = {
+      next: { month: nil, year: nil },
+      previous: { month: nil, year: nil }
+    }
+    
+    #find previous/next month/year if we have them                                   
+    if observation.dimensions.date
+      value = observation.dimensions.date.value
+      [:previous, :next].each do |axis|
+        concepts = value.attributes[axis]
+        concepts.each do |concept|
+          related_obs = Observation.find(:all, params: { series_id: params[:series],
+                                                         release_id: params[:release],
+                                                         dataset_id: params[:dataset], 
+                                                         product: observation.product,
+                                                         date: concept.value } )
+          related[ axis ][concept.period.to_sym] = related_obs.first
+        end
+      end  
+    end   
+    related
   end
   
   # start the server if ruby file executed directly
